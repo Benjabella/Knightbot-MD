f43f2112ca05958f91af0b6304f82a9cce3c6e8b
Benjamin Bella <33284320+Benjabella@users.noreply.github.com>
2025-04-03 21:41:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'Benjabella',
+  author: 'Bella',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Benjabella 🇰🇪', // Your name
+  ownerNumber: '254712280881', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
